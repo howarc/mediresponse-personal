7491fe86509a2d4e572b15003aa13cb741555bd6
howarch <howardchen187@gmail.com>
2024-05-08 00:06:56 -0400

    Introduced train/test split

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from torch import cuda
 import torch
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # init
 device = 'cuda' if cuda.is_available() else 'cpu'
@@ -20,11 +21,18 @@ def preprocess_data(file_path, output_file):
     df['dialogue'] = df['Input (Doctor)'].astype(str) + " <|endoftext|> " + df['Target (Relative)'].astype(str)
     df['dialogue'].to_csv(output_file, header=False, index=False, sep="\n")
 
+    train, test = train_test_split(df['dialogue'], test_size=0.3, random_state=42)
+    
+    # Save train and test sets to files
+    train.to_csv('train_dataset.txt', header=False, index=False, sep="\n")
+    test.to_csv('test_dataset.txt', header=False, index=False, sep="\n")
+
+
 
 preprocess_data('mediresponse.csv', 'preprocessed_conversation.txt')
 
-train_path = 'preprocessed_conversation.txt'
-test_path = 'preprocessed_conversation.txt'
+train_path = 'train_dataset.txt'
+test_path = 'test_dataset.txt'
 
 def load_dataset(train_path, test_path, tokenizer):
     train_dataset = TextDataset(
@@ -48,7 +56,7 @@ train_dataset, test_dataset, data_collator = load_dataset(train_path, test_path,
 training_args = TrainingArguments(
     output_dir='./results',
     overwrite_output_dir=True,
-    num_train_epochs=3,
+    num_train_epochs=40,
     per_device_train_batch_size=4,
     per_device_eval_batch_size=4,
     eval_steps=400,
@@ -84,6 +92,15 @@ def generate_text(prompt, max_length=100):
     response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
     return response
 
-prompt = "We've stabilized their vitals for now"
-response = generate_text(prompt)
+prompt1 = "A patient has been hospitalized because of a car accident. "
+prompt2 = "Play the role as a parent of this patient. The doctor tells you: They are currently in critical condition. "
+prompt3 = "As the parent, you are worried and ask: "
+response = generate_text(prompt1 + prompt2 + prompt3)
 print("Generated Response:", response)
+
+
+## Structure of a situation 
+# A patient has been hospitalized because of a [sports injury / car accident / heart attack / severe allergic reaction / severe burns / stroke ]
+# "You are [his/her/their] [friend/ parent/ grandparent/ boyfriend / girlfriend/ spouse / fiance / roommate / child / grandchild]."
+# As the doctor, you know their condition is [stable / critical / improving / detiorarting]/[passed away].
+##
